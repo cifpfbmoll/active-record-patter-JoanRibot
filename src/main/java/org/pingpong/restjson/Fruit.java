50a26d86728d3300ce3efc89da978b8002dbdb7a
JoanRibot <jribot@cifpfbmoll.eu>
2021-05-23 19:06:51 +0200

    empezando tablas y columnas echas en Fruits

--- src/main/java/org/pingpong/restjson/Fruit.java
+++ src/main/java/org/pingpong/restjson/Fruit.java
@@ -1,18 +1,28 @@
 package org.pingpong.restjson;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+@Table(name="Fruits")
 @JsonPropertyOrder({"name", "decription"})
-public class Fruit {
+public class Fruit extends PanacheEntity {
 
     // los propiedades han de ser publicas para que jackson
     // pueda acceder a ellar por reflection
     @NotBlank
+    @Column(unique = true)
     private String name;
+
     @NotEmpty
+    @Column()
     public String description;
 
     public Fruit() {
